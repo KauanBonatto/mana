3b939e6660d6886ced05ee191e56e8b8ecb9288a
mendesjar <ga0301@outlook.com>
2025-02-20 16:05:45 -0300

    style: game config export color type

--- src/GameConfig.ts
+++ src/GameConfig.ts
@@ -1,3 +1,5 @@
+export type pieceColor = 'BLACK' | 'WHITE';
+
 export interface ManaBoardProps {
   quantityMove: number;
   piece: piece | null;
@@ -5,9 +7,9 @@ export interface ManaBoardProps {
 }
 
 type piece = {
-  type: 'RONIN' | 'DAIMYO',
-  color: 'BLACK' | 'WHITE'
-}
+  type: 'RONIN' | 'DAIMYO';
+  color: pieceColor;
+};
 
 declare global {
   interface Window {
@@ -17,51 +19,195 @@ declare global {
 
 window.ManaBoard = [
   [
-    { quantityMove: 2, piece: {type: 'RONIN', color: 'BLACK'}, className: "bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 3, piece: {type: 'RONIN', color: 'BLACK'}, className: "bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 1, piece: {type: 'RONIN', color: 'BLACK'}, className: "bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 2, piece: {type: 'DAIMYO', color: 'BLACK'}, className: "bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 2, piece: {type: 'RONIN', color: 'BLACK'}, className: "bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 3, piece: {type: 'RONIN', color: 'BLACK'}, className: "bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover" },
+    {
+      quantityMove: 2,
+      piece: { type: 'RONIN', color: 'BLACK' },
+      className: 'bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 3,
+      piece: { type: 'RONIN', color: 'BLACK' },
+      className: 'bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 1,
+      piece: { type: 'RONIN', color: 'BLACK' },
+      className: 'bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 2,
+      piece: { type: 'DAIMYO', color: 'BLACK' },
+      className: 'bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 2,
+      piece: { type: 'RONIN', color: 'BLACK' },
+      className: 'bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 3,
+      piece: { type: 'RONIN', color: 'BLACK' },
+      className: 'bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover',
+    },
   ],
   [
-    { quantityMove: 2, piece: null, className: "bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 1, piece: null, className: "bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 3, piece: null, className: "bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 1, piece: null, className: "bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 3, piece: null, className: "bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 1, piece: null, className: "bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover" },
+    {
+      quantityMove: 2,
+      piece: null,
+      className: 'bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 1,
+      piece: null,
+      className: 'bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 3,
+      piece: null,
+      className: 'bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 1,
+      piece: null,
+      className: 'bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 3,
+      piece: null,
+      className: 'bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 1,
+      piece: null,
+      className: 'bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover',
+    },
   ],
   [
-    { quantityMove: 1, piece: null, className: "bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 3, piece: null, className: "bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 2, piece: null, className: "bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 3, piece: null, className: "bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 1, piece: null, className: "bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 2, piece: null, className: "bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover" },
+    {
+      quantityMove: 1,
+      piece: null,
+      className: 'bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 3,
+      piece: null,
+      className: 'bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 2,
+      piece: null,
+      className: 'bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 3,
+      piece: null,
+      className: 'bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 1,
+      piece: null,
+      className: 'bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 2,
+      piece: null,
+      className: 'bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover',
+    },
   ],
   [
-    { quantityMove: 3, piece: null, className: "bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 1, piece: null, className: "bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 2, piece: null, className: "bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 1, piece: null, className: "bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 3, piece: null, className: "bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 2, piece: null, className: "bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover" },
+    {
+      quantityMove: 3,
+      piece: null,
+      className: 'bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 1,
+      piece: null,
+      className: 'bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 2,
+      piece: null,
+      className: 'bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 1,
+      piece: null,
+      className: 'bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 3,
+      piece: null,
+      className: 'bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 2,
+      piece: null,
+      className: 'bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover',
+    },
   ],
   [
-    { quantityMove: 2, piece: null, className: "bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 3, piece: null, className: "bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 1, piece: null, className: "bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 3, piece: null, className: "bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 1, piece: null, className: "bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 3, piece: null, className: "bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover" },
+    {
+      quantityMove: 2,
+      piece: null,
+      className: 'bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 3,
+      piece: null,
+      className: 'bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 1,
+      piece: null,
+      className: 'bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 3,
+      piece: null,
+      className: 'bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 1,
+      piece: null,
+      className: 'bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 3,
+      piece: null,
+      className: 'bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover',
+    },
   ],
   [
-    { quantityMove: 2, piece: {type: 'RONIN', color: 'WHITE'}, className: "bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 1, piece: {type: 'RONIN', color: 'WHITE'}, className: "bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 3, piece: {type: 'RONIN', color: 'WHITE'}, className: "bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 2, piece: {type: 'DAIMYO', color: 'WHITE'}, className: "bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 2, piece: {type: 'RONIN', color: 'WHITE'}, className: "bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover" },
-    { quantityMove: 1, piece: {type: 'RONIN', color: 'WHITE'}, className: "bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover" },
+    {
+      quantityMove: 2,
+      piece: { type: 'RONIN', color: 'WHITE' },
+      className: 'bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 1,
+      piece: { type: 'RONIN', color: 'WHITE' },
+      className: 'bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 3,
+      piece: { type: 'RONIN', color: 'WHITE' },
+      className: 'bg-[url(./assets/move-3.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 2,
+      piece: { type: 'DAIMYO', color: 'WHITE' },
+      className: 'bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 2,
+      piece: { type: 'RONIN', color: 'WHITE' },
+      className: 'bg-[url(./assets/move-2.svg)] bg-no-repeat bg-cover',
+    },
+    {
+      quantityMove: 1,
+      piece: { type: 'RONIN', color: 'WHITE' },
+      className: 'bg-[url(./assets/move-1.svg)] bg-no-repeat bg-cover',
+    },
   ],
 ];
