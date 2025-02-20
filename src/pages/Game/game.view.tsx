0a03f06d21a6fb09b1dc295d392b92c15e04ff3b
mendesjar <ga0301@outlook.com>
2025-02-20 16:06:35 -0300

    feat: validation and create move piece

--- src/pages/Game/game.view.tsx
+++ src/pages/Game/game.view.tsx
@@ -1,18 +1,86 @@
-import { useState } from "react";
-import { useParams } from "react-router";
-import RONIN from "../../assets/pieces/ronin.svg"
-import DAIMYO from "../../assets/pieces/daimyo.svg"
-
-import type { ManaBoardProps } from "../../GameConfig";
+import { useState } from 'react';
+import { useParams } from 'react-router';
+import RONIN from '../../assets/pieces/ronin.svg';
+import DAIMYO from '../../assets/pieces/daimyo.svg';
+import type { ManaBoardProps, pieceColor } from '../../GameConfig';
 
 const GameView = () => {
   const { gameId } = useParams();
   const [board, setBoard] = useState<ManaBoardProps[][]>(window.ManaBoard);
+  const [capturedPieces, setCapturedPieces] = useState({
+    WHITE: [],
+    BLACK: [],
+  });
+  const [selectedPiece, setSelectedPiece] = useState<{
+    row: number;
+    col: number;
+  } | null>(null);
+  const [currentPlayer, setCurrentPlayer] = useState<pieceColor>('WHITE');
+
+  const movePiece = (
+    from: { row: number; col: number },
+    to: { row: number; col: number },
+  ) => {
+    const piece = board[from.row][from.col].piece;
+    if (!piece) return;
+
+    const targetCell = board[to.row][to.col];
+    if (targetCell.piece && targetCell.piece.color !== piece.color) {
+      setCapturedPieces((prevState) => ({
+        ...prevState,
+        [piece.color === 'WHITE' ? 'BLACK' : 'WHITE']: [
+          ...prevState[piece.color === 'WHITE' ? 'BLACK' : 'WHITE'],
+          targetCell.piece,
+        ],
+      }));
+    }
+
+    const newBoard = [...board];
+    newBoard[from.row][from.col].piece = null;
+    newBoard[to.row][to.col].piece = piece;
+
+    setBoard(newBoard);
+    setCurrentPlayer(currentPlayer === 'WHITE' ? 'BLACK' : 'WHITE');
+  };
+
+  const isValidMove = (
+    from: { row: number; col: number },
+    to: { row: number; col: number },
+  ) => {
+    const square = board[from.row][from.col];
+    if (!square) return;
+
+    const targetCell = board[to.row][to.col];
+
+    const moveDistance =
+      Math.abs(to.row - from.row) + Math.abs(to.col - from.col);
+    if (moveDistance !== square.quantityMove) return false;
+
+    if (targetCell.piece && targetCell.piece.color === square.piece?.color)
+      return false;
+
+    return true;
+  };
+
+  const handleCellClick = (row: number, col: number) => {
+    if (selectedPiece) {
+      const validMove = isValidMove(selectedPiece, { row, col });
+      if (validMove) {
+        movePiece(selectedPiece, { row, col });
+        setSelectedPiece(null);
+      }
+    } else {
+      const piece = board[row][col].piece;
+      if (piece) {
+        setSelectedPiece({ row, col });
+      }
+    }
+  };
 
   return (
     <div className="h-dvh w-screen bg-blue-500">
-      <div className="w-full h-full flex p-4">
-        <div className="flex flex-col w-full md:w-2/3">
+      <div className="flex h-full w-full p-4">
+        <div className="flex w-full flex-col md:w-2/3">
           <div className="flex flex-none gap-x-2 font-normal">
             <img
               width={50}
@@ -21,10 +89,10 @@ const GameView = () => {
             />
             <p>Fulano</p>
           </div>
-          <div className="mana-board w-full flex flex-col bg-blue-800 flex-grow max-h-full">
-            <div className="relative w-full h-full max-h-full">
-              <table className="h-auto w-full sm:w-auto sm:h-full aspect-square bg-white">
-                <tbody className="flex flex-col h-full">
+          <div className="mana-board flex max-h-full w-full flex-grow flex-col bg-blue-800">
+            <div className="relative h-full max-h-full w-full">
+              <table className="aspect-square h-auto w-full bg-white sm:h-full sm:w-auto">
+                <tbody className="flex h-full flex-col">
                   {board.map((row, rowIndex) => (
                     <tr key={`row-${rowIndex}`} className="row flex flex-1">
                       {row.map((column, columnIndex) => (
@@ -33,12 +101,16 @@ const GameView = () => {
                           className="column flex-1 p-0"
                         >
                           <div
-                            className={`square w-full h-full flex items-center justify-center bg-blue-900 text-white ${column.className}`}
-                            onClick={() => console.log(column)}
+                            className={`square flex h-full w-full items-center justify-center bg-blue-900 ${selectedPiece && selectedPiece?.row === rowIndex && selectedPiece?.col === columnIndex ? 'bg-blue-900/90' : ''} text-white ${column.className}`}
+                            onClick={() =>
+                              handleCellClick(rowIndex, columnIndex)
+                            }
                           >
                             {column.piece && (
                               <img
-                                src={column.piece.type === 'RONIN' ? RONIN : DAIMYO}
+                                src={
+                                  column.piece.type === 'RONIN' ? RONIN : DAIMYO
+                                }
                                 className={`w-5 ${column.piece.color === 'BLACK' ? 'brightness-0' : ''}`}
                                 alt={column.piece.type}
                               />
@@ -65,7 +137,7 @@ const GameView = () => {
           <h1 className="text-white">{`GameID is: ${gameId}`}</h1>
         </div>
       </div>
-    </div >
+    </div>
   );
 };
 
